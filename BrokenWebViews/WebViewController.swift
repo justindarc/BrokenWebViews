045cbcae6eb40bad4a7b846235bb99d7c1fc4bf1
Justin D'Arcangelo <justindarc@gmail.com>
2018-02-07 14:45:05 -0500

    Added 'Reload' button.

--- BrokenWebViews/WebViewController.swift
+++ BrokenWebViews/WebViewController.swift
@@ -11,16 +11,31 @@ import WebKit
 
 class WebViewController: UIViewController {
     let webView = WKWebView()
+    let reloadButton = UIButton(type: .system)
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         webView.translatesAutoresizingMaskIntoConstraints = false
 
+        reloadButton.frame = CGRect(x: 10, y: 30, width: 60, height: 40)
+        reloadButton.layer.cornerRadius = 5
+        reloadButton.layer.borderColor = UIColor.black.cgColor
+        reloadButton.layer.borderWidth = 1
+        reloadButton.backgroundColor = UIColor.white
+        reloadButton.setTitle("Reload", for: .normal)
+        reloadButton.addTarget(self, action: #selector(reload), for: .touchUpInside)
+
         view.addSubview(webView)
+        view.addSubview(reloadButton)
+
         view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "|[webView]|", options: .alignAllCenterX, metrics: nil, views: ["webView": webView]))
         view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[webView]|", options: .alignAllCenterY, metrics: nil, views: ["webView": webView]))
 
         webView.load(URLRequest(url: URL(string: "http://www.eurogamer.net/articles/2017-07-14-zelda-breath-of-the-wild-captured-memories-locations-4857")!))
+    }
+
+    @objc func reload() {
+        webView.reload()
     }
 }
